81bf78ea5e9496ec9dc285d6caf08fb2b3ebd16e
Per Baltzar <42942103+perbaltzar@users.noreply.github.com>
2019-05-03 21:37:24 +0200

    added maptiles with collisions to game

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,20 @@
 import Phaser from 'phaser';
 
-import sky from './assets/sky.png';
+// Maps
+import mapTest from './assets/maps/inca.json';
+import inca from './assets/maps/inca_front.png';
 
-import platform from './assets/platform.png';
-import star from './assets/star.png';
-import bomb from './assets/bomb.png';
-import dude from './assets/dude.png';
+// Backgrounds
+import background from './assets/backgrounds/background.png';
+
+// Sprites
+import star from './assets/sprites/star.png';
+import dude from './assets/sprites/dude.png';
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
-  height: 600,
+  height: 640,
   physics: {
     default: 'arcade',
     arcade: {
@@ -25,117 +29,97 @@ const config = {
   }
 };
 
-const game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image('sky', sky);
-  this.load.image('ground', platform);
-  this.load.image('star', star);
-  this.load.image('bomb', bomb);
-  this.load.spritesheet('dude', dude, {
-    frameWidth: 32,
-    frameHeight: 48
-  });
+    this.load.tilemapTiledJSON('map', mapTest);
+    this.load.spritesheet('inca', inca, {
+        frameWidth: 16,
+        frameHeight: 16
+    });
+    this.load.image('background', background);
+    this.load.image('star', star);
+    this.load.spritesheet('dude', dude, {
+        frameWidth: 32,
+        frameHeight: 48
+    });
+   
 }
 
-let platforms;
-let stars;
+let map;
 let player;
 let cursors;
-
-var score = 0;
-var scoreText;
+let groundLayer
+var text;
+let bg;
 
 function create() {
-  this.add.image(400, 300, 'sky');
+    // BACKGROUND
+    bg = this.add.tileSprite(100, 100, 0, 0, 'background');
 
-  // PLATFORMS
-  platforms = this.physics.add.staticGroup();
+    // MAP
+    map = this.make.tilemap({
+        key: 'map'
+    });
+    var groundTiles = map.addTilesetImage('inca');
+    groundLayer = map.createDynamicLayer('layer', groundTiles, 0, 0);
 
-  platforms
-    .create(400, 568, 'ground')
-    .setScale(2)
-    .refreshBody();
 
-  platforms.create(600, 400, 'ground');
-  platforms.create(50, 250, 'ground');
-  platforms.create(750, 220, 'ground');
+    
 
-  // STARS
-  stars = this.physics.add.group({
-    key: 'star',
-    repeat: 11,
-    setXY: { x: 12, y: 0, stepX: 70 }
-  });
+    // PLAYER
+    player = this.physics.add.sprite(240, 450, 'dude');
 
-  stars.children.iterate(function(child) {
-    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  });
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    this.physics.add.collider(player, groundLayer);
+    groundLayer.setCollision([0, 1, 22, 23, 26, 27, 28, 29, 32, 33, 38, 39, 40, 41, 60, 61, 70, 71, 100, 110, 120, 130])
 
-  this.physics.add.collider(stars, platforms);
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', {
+            start: 0,
+            end: 3
+        }),
+        frameRate: 10,
+        repeat: -1
+    });
 
-  // PLAYER
-  player = this.physics.add.sprite(100, 450, 'dude');
+    this.anims.create({
+        key: 'turn',
+        frames: [{
+            key: 'dude',
+            frame: 4
+        }],
+        frameRate: 20
+    });
 
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  this.anims.create({
-    key: 'turn',
-    frames: [{ key: 'dude', frame: 4 }],
-    frameRate: 20
-  });
-
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  this.physics.add.collider(player, platforms);
-
-  // SCORE
-  scoreText = this.add.text(16, 16, 'score: 0', {
-    fontSize: '32px',
-    fill: '#000'
-  });
-}
-
-function collectStar(player, star) {
-  star.disableBody(true, true);
-
-  score += 10;
-  scoreText.setText('Score: ' + score);
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', {
+            start: 5,
+            end: 8
+        }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 function update() {
-  cursors = this.input.keyboard.createCursorKeys();
+    cursors = this.input.keyboard.createCursorKeys();
 
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
 
-    player.anims.play('left', true);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-
-    player.anims.play('right', true);
-  } else {
-    player.setVelocityX(0);
-
-    player.anims.play('turn');
-  }
-
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
-  }
-
-  this.physics.add.overlap(player, stars, collectStar, null, this);
+    if (cursors.up.isDown) {
+        player.setVelocityY(-330);
+    }
 }
