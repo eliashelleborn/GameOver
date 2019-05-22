c214b16118e517625d84801b31f2e63254e4a62a
Elias Johansson <elias_johansson@hotmail.se>
2019-05-22 11:46:31 +0200

    Prevent health from going under zero

--- packages/server/src/models/Player.js
+++ packages/server/src/models/Player.js
@@ -2,12 +2,13 @@ class Player {
   constructor(id, name) {
     this.id = id; // Socket ID
     this.name = name;
-    this.health = 20;
+    this.health = 0;
     this.alive = true;
   }
 
   updateHealth(healthChange) {
     this.health += healthChange;
+    if (this.health < 0) this.health = 0;
   }
 
   die() {
