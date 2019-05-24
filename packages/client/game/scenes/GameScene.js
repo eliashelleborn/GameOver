6ecf05ddb90b6858686ef10f59bfbe4c6d936aba
Elias Johansson <elias_johansson@hotmail.se>
2019-05-24 12:33:53 +0200

    Reset test game settings

--- packages/client/game/scenes/GameScene.js
+++ packages/client/game/scenes/GameScene.js
@@ -61,7 +61,7 @@ class GameScene extends Phaser.Scene {
     this.gameState.players.forEach((p) => {
       // Randomize Spawn Position
       const randomNumber = Phaser.Math.Between(0, this.spawnPoints.length - 1);
-      const spawnPoint = this.spawnPoints[1];
+      const spawnPoint = this.spawnPoints[randomNumber];
 
       const player = new Player({
         scene: this,
