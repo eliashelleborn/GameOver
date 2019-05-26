603729ab2535aa1b83c4a2fe71ee608c5292fe0c
Elias Johansson <elias_johansson@hotmail.se>
2019-05-26 12:52:58 +0200

    Lobby scroll fix

--- packages/client/app/pages/Game/Lobby.js
+++ packages/client/app/pages/Game/Lobby.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useStore } from 'easy-peasy';
+import Div100vh from 'react-div-100vh/lib/Div100vh';
 import RoomCode from '../../components/Lobby/RoomCode';
 import LobbyBox from '../../components/Lobby/LobbyBox';
 import Players from '../../components/Lobby/Players';
 import GameInfo from '../../components/Lobby/GameInfo';
 
-const StyledLobby = styled.div`
+const StyledLobby = styled(Div100vh)`
   padding: 2rem;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   position: relative;
+  overflow-y: scroll;
 `;
 
 const Lobby = () => {
