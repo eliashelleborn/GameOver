377ef2edb373337544f6fb7c0a1c7f61a32acd50
Per Baltzar <42942103+perbaltzar@users.noreply.github.com>
2019-05-31 09:33:28 +0200

    fix design bug

--- packages/client/app/components/Lobby/HowToPlay.js
+++ packages/client/app/components/Lobby/HowToPlay.js
@@ -4,6 +4,7 @@ import Instructions from './Instructions';
 
 const StyledHowToPlay = styled.div`
   width: 100%;
+  max-width: 350px;
   margin-top: 1rem;
   border-top: 2px solid ${({ theme }) => theme.colorPrimary};
   button {
