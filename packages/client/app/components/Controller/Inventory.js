4022f9efc88e072374256c3fd0407ed20fea8126
Per Baltzar <42942103+perbaltzar@users.noreply.github.com>
2019-06-10 13:01:08 +0200

    landscape css

--- packages/client/app/components/Controller/Inventory.js
+++ packages/client/app/components/Controller/Inventory.js
@@ -4,8 +4,9 @@ import styled from 'styled-components';
 import Weapon from './Weapon';
 
 const StyledInventory = styled.div`
-  width: 90%;
-  transform: translateY(-300px);
+  width: 100%;
+  top: auto;
+  bottom: 126px;
   background-color: ${({ theme }) => theme.colorPrimary};
   position: absolute;
   padding: 1rem;
@@ -24,17 +25,26 @@ const StyledInventory = styled.div`
       margin: 0;
     }
   }
+  @media screen and (orientation: landscape) and (max-height: 500px) {
+    top: 87px;
+    bottom: auto;
+    width: 95%;
+  }
 `;
 const StyledWeaponGrid = styled.div`
   display: grid !important;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 1rem;
+  @media screen and (orientation: landscape) and (max-height: 500px) {
+    grid-template-columns: repeat(9, 1fr);
+  }
 `;
 
 const Inventory = ({ inventory, selectedWeapon, selectWeapon }) => (
   <StyledInventory>
     <div>
-      <p>Chosen Weapon</p>
+      <p> Chosen Weapon </p>
+
       <h4>{selectedWeapon.name}</h4>
     </div>
     <StyledWeaponGrid>
