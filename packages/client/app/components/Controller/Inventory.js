acdb3680fb8fc978ae515177d8c02a1cc52ed43b
Per Baltzar <42942103+perbaltzar@users.noreply.github.com>
2019-06-10 14:08:18 +0200

    updated design

--- packages/client/app/components/Controller/Inventory.js
+++ packages/client/app/components/Controller/Inventory.js
@@ -4,49 +4,55 @@ import styled from 'styled-components';
 import Weapon from './Weapon';
 
 const StyledInventory = styled.div`
-  width: 100%;
+  width: 92%;
   top: auto;
   bottom: 126px;
+  margin: 0;
   background-color: ${({ theme }) => theme.colorPrimary};
   position: absolute;
   padding: 1rem;
   z-index: 1000;
   border-radius: 5px;
   flex-flow: column;
-  div:nth-child(1) {
-    display: flex;
-    flex-flow: column;
-    align-items: center;
-    height: 55px;
-    border-radius: 5px;
-    background: white;
-    margin-bottom: 1rem;
-    p {
-      margin: 0;
-    }
-  }
+
   @media screen and (orientation: landscape) and (max-height: 500px) {
+    width: 100%;
     top: 87px;
     bottom: auto;
     width: 95%;
   }
 `;
+
+const StyledChosenWeapon = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  height: 55px;
+  border-radius: 5px;
+  background: white;
+  margin-bottom: 1rem;
+  p {
+    margin: 0;
+  }
+`;
+
 const StyledWeaponGrid = styled.div`
   display: grid !important;
   grid-template-columns: repeat(4, 1fr);
-  grid-gap: 1rem;
+  grid-gap: 5%;
   @media screen and (orientation: landscape) and (max-height: 500px) {
     grid-template-columns: repeat(9, 1fr);
+    grid-gap: 2%;
   }
 `;
 
 const Inventory = ({ inventory, selectedWeapon, selectWeapon }) => (
   <StyledInventory>
-    <div>
+    <StyledChosenWeapon>
       <p> Chosen Weapon </p>
 
       <h4>{selectedWeapon.name}</h4>
-    </div>
+    </StyledChosenWeapon>
     <StyledWeaponGrid>
       {inventory.map(weapon => (
         <Weapon
