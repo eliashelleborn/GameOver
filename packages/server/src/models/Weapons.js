2f7a9dab7dde6b444f9b0a71eb0744b9d63b43ed
Per Baltzar <42942103+perbaltzar@users.noreply.github.com>
2019-06-11 12:51:00 +0200

    Weapons Model on server

---
+++ packages/server/src/models/Weapons.js
@@ -0,0 +1,20 @@
+class Weapons {
+  constructor() {
+    this.list = [{
+      type: 'Bazooka',
+      name: 'Bazooka',
+      ammo: 10,
+      key: 'bazooka',
+      image: '../../images/bazooka.png',
+    },
+    {
+      type: 'GrenadeLauncher',
+      name: 'Grenade Launcher',
+      ammo: 10,
+      key: 'grenadelauncher',
+      image: '../../images/grenadelauncher.png',
+    },
+    ];
+  }
+}
+export default Weapons;
