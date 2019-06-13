4148e4a3745eaf6c05827d6acd8b4bc6de6b240d
Per Baltzar <42942103+perbaltzar@users.noreply.github.com>
2019-06-13 11:40:20 +0200

    themed messages

--- packages/client/app/components/Controller/FlashMessage.js
+++ packages/client/app/components/Controller/FlashMessage.js
@@ -3,15 +3,18 @@ import styled from 'styled-components';
 
 const StyledFlashMessage = styled.div`
   border-radius: 5px;
-  background-color: ${({ theme }) => theme.colorSecondaryHover};
+  background-color: ${({ theme, type }) => (type === 'pickup' && theme.colorPickUp) || (type === 'hurt' && theme.colorHurt)};
   padding: 1rem;
   display: flex;
   align-items: center;
   margin-bottom: 1rem;
+  transition: 0.3s ease-in;
 `;
 
 const FlashMessage = ({ message, toggleFlashMessage }) => (
-  <StyledFlashMessage onClick={() => toggleFlashMessage(message)}>{message}</StyledFlashMessage>
+  <StyledFlashMessage type={message.type} onClick={() => toggleFlashMessage(message)}>
+    {message.message}
+  </StyledFlashMessage>
 );
 
 export default FlashMessage;
--- packages/client/app/style/theme.js
+++ packages/client/app/style/theme.js
@@ -3,6 +3,8 @@ const theme = {
   colorPrimaryHover: '#5a698b',
   colorSecondary: '#FFCD55',
   colorSecondaryHover: '#FFDA83',
+  colorHurt: '#bf3b3b',
+  colorPickUp: '#bfe7a3',
 };
 
 export default theme;
